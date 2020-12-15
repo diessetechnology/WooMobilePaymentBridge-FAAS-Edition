34c49dd4f69a41697767b346a48e109196bb3315
Davide Salamino <davide.salamino@diessetechnology.it>
2020-12-15 02:56:47 +0100

    Corretti errori. Rimossa una parte inutile del codice

--- handler.js
+++ handler.js
@@ -10,25 +10,17 @@ module.exports = async (event, context) => {
         headers: {'X-Custom-Header': 'foobar'}
     });
 
+    const price =  ((event.body.amount)*100);
+
     const intent = await stripe.paymentIntents.create({
         payment_method: event.body.paymentMethod,
-        amount: event.body.amount,
+        amount: price,
         currency: 'eur',
         payment_method_types: ['card'],
         confirm: true,
         confirmation_method: "automatic",
         receipt_email: event.body.receipt_email,
         capture_method: "automatic"
-    }).then(async (res) => {
-        if (res.status === "succeeded")
-        {
-            await woo.post("wp-json/wc/v3/orders/" + id, {}, {
-                auth: {
-                    username: process.env.WOOAPICK,
-                    password: process.env.WOOAPICS
-                }
-            }).then().catch().done()
-        }
     })
-    return {status: intent.id}
+    return {status: intent.status}
 }
